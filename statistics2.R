a5a156306f548f5a59649b1f164b8705280ebcde
4Jatinyadav <44647223+4Jatinyadav@users.noreply.github.com>
2019-05-06 22:52:59 +0530

    added calculation using R var and std

---
+++ statistics2.R
@@ -0,0 +1,12 @@
+#1to calculate the variance and standered deviation
+s<-c(17,15,23,7,9,13)
+m<-var(s)
+m
+std<-sqrt(m)
+std
+#2to calculate the variance and stadered deviation
+s1<-c(74,122,235,111,292,111,211,133,156,79)
+m1<-var(s1)
+m1
+std1<-sqrt(m1)
+std1
